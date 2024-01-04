a19e1d83a846310c462898fd72223974b5a44c31
이문빈 <3412mb@gmail.com>
2024-01-04 18:12:41 +0900

    실패, 파괴 열 추가

--- starforce.js
+++ starforce.js
@@ -5,12 +5,14 @@ function get_success_rate(star) {
     return 25 - star
 }
 
-function get_keep_rate(star) {
-    return 0
-}
-
 function get_destroy_rate(star) {
-    return 0
+    if (star <= 14) return 0
+    if (star <= 17) return 2.1
+    if (star <= 19) return 2.8
+    if (star <= 21) return 7
+    if (star == 22) return 19.4
+    if (star == 23) return 29.4
+    return 39.6
 }
 
 class Starforce {
@@ -19,10 +21,10 @@ class Starforce {
         this.count = 0
         this.success_rate = get_success_rate(star)
         this.success_observed = 0
-        this.keep_rate = get_keep_rate(star)
-        this.keep_observed = 0
         this.destory_rate = get_destroy_rate(star)
         this.destory_observed = 0
+        this.fail_rate = 100 - get_success_rate(star) - get_destroy_rate(star)
+        this.fail_observed = 0
     }
 }
 
@@ -38,8 +40,16 @@ starforces.slice().reverse().forEach((starforce) => {
     let row = "<tr align='center'>"
     row += "<td>" + starforce.star + " -> " + (starforce.star + 1) + "</td>";
     row += "<td>" + starforce.count + "</td>"
+
     row += "<td>" + starforce.success_rate.toFixed(2) + "% , " + (starforce.count * starforce.success_rate / 100).toFixed(2) + "회" + "</td>"
     row += "<td>" + (starforce.success_observed / starforce.count).toFixed(2) + "% , " + starforce.success_observed + "회" + "</td>"
+
+    row += "<td>" + starforce.fail_rate.toFixed(2) + "% , " + (starforce.count * starforce.fail_rate / 100).toFixed(2) + "회" + "</td>"
+    row += "<td>" + (starforce.fail_observed / starforce.count).toFixed(2) + "% , " + starforce.fail_observed + "회" + "</td>"
+
+    row += "<td>" + starforce.destory_rate.toFixed(2) + "% , " + (starforce.count * starforce.destory_rate / 100).toFixed(2) + "회" + "</td>"
+    row += "<td>" + (starforce.destory_observed / starforce.count).toFixed(2) + "% , " + starforce.destory_observed + "회" + "</td>"
+
     row += "</tr>"
 
     starforce_tbody.insertAdjacentHTML('afterend', row)
